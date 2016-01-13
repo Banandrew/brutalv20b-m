4f0731b1ad32ddc98fa5037fc61839b7ccc5bb04
Andrew Golubev <aagolubev@gmail.com>
2016-01-13 22:46:46 +0300

    Initial commit.

---
+++ compile.bat
@@ -0,0 +1,44 @@
+@echo off
+
+SET ZLOC="%~dp0\tools\7za.exe"
+
+
+if a%1 == a goto error
+
+if a%2 == a goto defaultname
+set PKNAME=%2
+goto continue
+
+:defaultname
+set PKNAME=brutalv20b-m.pk3
+
+:continue
+
+echo Going to compress this folder to pk3:
+
+cd %1
+
+cd
+
+echo.
+
+%ZLOC% u -r -tzip ..\%PKNAME% . -x!*.dbs -x!*.bak -x!*.backup* -x!*Thumbs.db
+
+goto end
+
+:error
+echo Missing argument!
+echo usage:  compile.bat folder [output.pk3]
+echo.
+rem pause
+goto veryendnocdup
+
+:end
+echo.
+echo Done.
+
+:veryend
+cd ..
+
+:veryendnocdup
+pause
